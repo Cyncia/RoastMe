83892ff6c3b7737cc12b7ef78112dc75e5be3de8
alexanderqchen <aqchen@ucla.edu>
2018-02-22 15:20:00 -0800

    created api file

---
+++ Play/api.swift
@@ -0,0 +1,62 @@
+//
+//  api.swift
+//  RoastMe
+//
+//  Created by Alexander Chen on 2/21/18.
+//  Copyright © 2018 Cynthia Zhou. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+func getRandomRoasts(Post) {
+    
+}
+
+func upvote(Roast) {
+    
+}
+
+func unUpvote(Roast) {
+    
+}
+
+func getAllRoasts(Post) {
+    
+}
+
+func sendRoast(Post) {
+    
+}
+
+func getNextPost() {
+    
+}
+
+func getPrevPost() {
+    
+}
+
+func createPost() {
+    
+}
+
+func getUserInfo() {
+    
+}
+
+func getYourRoasts() {
+    
+}
+
+func getYourPosts() {
+    
+}
+
+func getPostFromRoast() {
+    
+}
+
+func getUserFromRoast() {
+    
+}
