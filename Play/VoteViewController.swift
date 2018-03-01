2a439000792413783dfd622acf97a6fd85d12d66
Cyncia <cynciazhou@gmail.com>
2018-02-28 18:33:39 -0800

    Transition and Table Added

--- Play/VoteViewController.swift
+++ Play/VoteViewController.swift
@@ -17,20 +17,79 @@ class VoteViewController: UIViewController {
     @IBOutlet var Roast4: UIView!
     @IBOutlet var Roast5: UIView!
     
-   
+    /*
+    var interactor:Interactor? = nil
+    @IBAction func handleGesture(_ sender: UIPanGestureRecognizer) {
+        let percentThreshold:CGFloat = 0.6
+        
+        // convert y-position to downward pull progress (percentage)
+        let translation = sender.translation(in: view)
+        let verticalMovement = translation.y / view.bounds.height
+        let downwardMovement = fmaxf(Float(verticalMovement), 0.0)
+        let downwardMovementPercent = fminf(downwardMovement, 1.0)
+        let progress = CGFloat(downwardMovementPercent)
+        
+        guard let interactor = interactor else { return }
+        
+        switch sender.state {
+        case .began:
+            interactor.hasStarted = true
+            dismiss(animated: true, completion: nil)
+        case .changed:
+            interactor.shouldFinish = progress > percentThreshold
+            interactor.update(progress)
+        case .cancelled:
+            interactor.hasStarted = false
+            interactor.cancel()
+        case .ended:
+            interactor.hasStarted = false
+            interactor.shouldFinish
+                ? interactor.finish()
+                : interactor.cancel()
+        default:
+            break
+        }
+    } // end of HandleGesture
+    */
+    
+    // define a variable to store initial touch position
+    var initialTouchPoint: CGPoint = CGPoint(x: 0,y: 0)
+    
+    @IBAction func panGestureRecognizerHandler(_ sender: UIPanGestureRecognizer) {
+        let touchPoint = sender.location(in: self.view?.window)
+        
+        if sender.state == UIGestureRecognizerState.began {
+            initialTouchPoint = touchPoint
+        } else if sender.state == UIGestureRecognizerState.changed {
+            if touchPoint.y - initialTouchPoint.y > 0 {
+                self.view.frame = CGRect(x: 0, y: touchPoint.y - initialTouchPoint.y, width: self.view.frame.size.width, height: self.view.frame.size.height)
+            }
+        } else if sender.state == UIGestureRecognizerState.ended || sender.state == UIGestureRecognizerState.cancelled {
+            if touchPoint.y - initialTouchPoint.y > 100 {
+                self.dismiss(animated: true, completion: nil)
+            } else {
+                UIView.animate(withDuration: 0.3, animations: {
+                    self.view.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height)
+                })
+            }
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
         
-        Roast1.layer.cornerRadius = 10.0
+        Roast1.layer.cornerRadius = 15.0
         Roast1.clipsToBounds = true
-        Roast2.layer.cornerRadius = 10.0
+        Roast2.layer.cornerRadius = 15.0
         Roast2.clipsToBounds = true
-        Roast3.layer.cornerRadius = 10.0
+        Roast3.layer.cornerRadius = 15.0
         Roast3.clipsToBounds = true
-        Roast4.layer.cornerRadius = 10.0
+        Roast4.layer.cornerRadius = 15.0
         Roast4.clipsToBounds = true
-        Roast5.layer.cornerRadius = 10.0
+        Roast5.layer.cornerRadius = 15.0
         Roast5.clipsToBounds = true
         
         
