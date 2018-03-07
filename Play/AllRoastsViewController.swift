2ffb9988b14b572cf6d2f99d410d513fc12fbb25
alexanderqchen <aqchen@ucla.edu>
2018-03-07 02:21:18 -0800

    cant figure out how to use swipeMenuController with tableViews, so saving this copy here and will make changes for hacksprint on master

--- Play/AllRoastsViewController.swift
+++ Play/AllRoastsViewController.swift
@@ -12,12 +12,9 @@ class AllRoastsViewController: UIViewController {
 
     var history = [String]()
     var curr = -1
-    var roastTexts = ["hello"]
+    var roastTexts = [String]()
     
     @IBOutlet var Chart: UITableView!
-    
-    var users: [User] = [User(image: #imageLiteral(resourceName: "Person1"), name: "name", roast: "hello")]
-    let roasts = ["There are 10 kinds of people in the world: those who understand binary, and those who don't.", "int elligence;", "Knock, knock. Race condition. Who’s there.", "%rip", "Two bytes meet. The first byte asks, “Are you ill?” The second byte replies, “No, just feeling a bit off."]
     
     override func viewWillAppear(_ animated: Bool) {
         getAllRoasts(postId: history[curr]) { (roasts) in
@@ -40,17 +37,6 @@ class AllRoastsViewController: UIViewController {
         Chart.delegate = self
         Chart.dataSource = self
     }
-
-    // creating data and returning it as an array
-    func createArray() -> [User] {
-        var temp: [User] = []
-        
-        for roastText in roastTexts {
-            temp.append(User(image: #imageLiteral(resourceName: "Person1"), name: "hello", roast: roastText))
-        }
-        print(roastTexts)
-        return temp
-    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -71,28 +57,8 @@ class AllRoastsViewController: UIViewController {
 extension AllRoastsViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print("users.count: ", users.count)
         return roastTexts.count
     }
-    
-   // func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-    //    return 90
-    //}
-    
-    /*
-    // Change Selection Color
-     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let selectedCell:UITableViewCell = tableView.cellForRow(at: indexPath)!
-        selectedCell.contentView.backgroundColor = UIColor.red
-    }
-    
-    // Just set it back in deselect
-    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
-        let cellToDeSelect:UITableViewCell = tableView.cellForRow(at: indexPath)!
-        cellToDeSelect.contentView.backgroundColor = UIColor.clear
-    }
-    */
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // Configure the cell...
@@ -102,7 +68,6 @@ extension AllRoastsViewController: UITableViewDataSource, UITableViewDelegate {
         
         roastCell.cellView.layer.borderWidth = 0.4
         roastCell.cellView.layer.borderColor = UIColor.lightGray.cgColor
-        // player_Cell.cellView.layer.cornerRadius = player_Cell.cellView.frame.height / 2.5
         
         
         roastCell.setText(roastText: roast)
--- Play/CustomViewController.swift
+++ Play/CustomViewController.swift
@@ -10,30 +10,12 @@ import UIKit
 import SwipeMenuViewController
 
 class CustomViewController: SwipeMenuViewController {
-    /*
-    @IBOutlet weak var swipeMenuView: SwipeMenuView!
-    {
-        didSet {
-            swipeMenuView.delegate                        = self
-            swipeMenuView.dataSource                      = self
-            var options: SwipeMenuViewOptions             = .init()
-            options.tabView.style                         = .segmented
-            options.tabView.underlineView.backgroundColor = UIColor.red
-            options.tabView.itemView.textColor            = UIColor.blue
-            options.tabView.itemView.selectedTextColor    = UIColor.green
-            swipeMenuView.reloadData(options: options)
-        }
-    }
-    */
     
-    // shoud change leaderboard to something else
-    let tabs = ["Posts", "Roasts", "Other"]
+    let tabs = ["Posts", "Roasts"]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // swipeMenuView.layout
         
         swipeMenuView.delegate                        = self
         swipeMenuView.dataSource                      = self
@@ -43,59 +25,30 @@ class CustomViewController: SwipeMenuViewController {
         options.tabView.itemView.textColor            = UIColor.lightGray
         options.tabView.itemView.selectedTextColor    = UIColor(red:214/255.0, green:69/255.0, blue:65/255.0, alpha:1.0)
         
-        // UIFont (name: "Futura Medium", size: 10)
-        // for testing
-        // options.tabView.backgroundColor = .red
         swipeMenuView.reloadData(options: options)
         
-        /*
-        ProfilePic.layer.borderWidth = 2
-        ProfilePic.layer.masksToBounds = false
-        ProfilePic.layer.borderColor = UIColor.white.cgColor
-        ProfilePic.layer.cornerRadius = ProfilePic.frame.height/2
-        ProfilePic.clipsToBounds = true
-        */
-        
-        //swipeMenuView.dataSource = self
-        //swipeMenuView.delegate = self
-        //let options: SwipeMenuViewOptions = .init()
-        //swipeMenuView.reloadData(options: options)
-        
     }
-//}
 
-//extension CustomViewController {
-    
-    // MARK: - SwipeMenuViewDelegate
     /// Called before setup self.
     override func swipeMenuView(_ swipeMenuView: SwipeMenuView, viewWillSetupAt currentIndex: Int) {
         print("will setup SwipeMenuView")
-        // Codes
     }
     
     /// Called after setup self.
     override func swipeMenuView(_ swipeMenuView: SwipeMenuView, viewDidSetupAt currentIndex: Int) {
         print("did setup SwipeMenuView")
-        // Codes
     }
     
     // Called before swiping the page.
     override func swipeMenuView(_ swipeMenuView: SwipeMenuView, willChangeIndexFrom fromIndex: Int, to toIndex: Int) {
         print("will change from section\(fromIndex + 1)  to section\(toIndex + 1)")
-        // toIndex page
-        
-        // Codes
     }
     
     // Called after swiping the page.
     override func swipeMenuView(_ swipeMenuView: SwipeMenuView, didChangeIndexFrom fromIndex: Int, to toIndex: Int) {
         print("did change from section\(fromIndex + 1)  to section\(toIndex + 1)")
-        // Codes
     }
     
-    
-    // MARK - SwipeMenuViewDataSource
-    // Return the number of pages in `SwipeMenuView`.
     override func numberOfPages(in swipeMenuView: SwipeMenuView) -> Int {
         return tabs.count
     }
@@ -107,74 +60,8 @@ class CustomViewController: SwipeMenuViewController {
     
     // Return a ViewController to be displayed at the specified page in `SwipeMenuView`.
     override func swipeMenuView(_ swipeMenuView: SwipeMenuView, viewControllerForPageAt index: Int) -> UIViewController {
-        /*
-        var vc: UIViewController
-        
-        if index == 0 {
-            vc = PostsVC()
-            print("VC: 0")
-        }
-        else if index == 1 {
-            vc = RoastsVC()
-            print("VC: 1")
-        }
-        // if index == 2
-        else {
-            vc = OtherVC()
-            print("VC: 2")
-        }
- */
-
         let vc = ContentViewController()
-        vc.content = tabs[index]
-        
+        vc.Chart.reloadData()
         return vc
     }
 }
-
-
-/*
- // USE THIS IF YOU ARE USING UIVIEW & HAVING A SWIPEMENU VAR
-extension CustomViewController: SwipeMenuViewDelegate {
-    // MARK - SwipeMenuViewDelegate
-    /// Called before setup self.
-    func swipeMenuView(_ swipeMenuView: SwipeMenuView, viewWillSetupAt currentIndex: Int) {
-        // Codes
-    }
-    
-    /// Called after setup self.
-    func swipeMenuView(_ swipeMenuView: SwipeMenuView, viewDidSetupAt currentIndex: Int) {
-        // Codes
-    }
-    
-    // Called before swiping the page.
-    func swipeMenuView(_ swipeMenuView: SwipeMenuView, willChangeIndexFrom fromIndex: Int, to toIndex: Int) {
-        // Codes
-    }
-    
-    // Called after swiping the page.
-    func swipeMenuView(_ swipeMenuView: SwipeMenuView, didChangeIndexFrom fromIndex: Int, to toIndex: Int) {
-        // Codes
-    }
-}
-
-extension CustomViewController: SwipeMenuViewDataSource {
-    
-    //MARK - SwipeMenuViewDataSource
-    // Return the number of pages in `SwipeMenuView`.
-    func numberOfPages(in swipeMenuView: SwipeMenuView) -> Int {
-        return tabs.count
-    }
-    
-    // Return strings to be displayed at the specified tag in `SwipeMenuView`.
-    func swipeMenuView(_ swipeMenuView: SwipeMenuView, titleForPageAt index: Int) -> String {
-        return tabs[index]
-    }
-    
-    // Return a ViewController to be displayed at the specified page in `SwipeMenuView`.
-    func swipeMenuView(_ swipeMenuView: SwipeMenuView, viewControllerForPageAt index: Int) -> UIViewController {
-        let vc = RoastViewController()
-        return vc
-    }
-}
-*/
